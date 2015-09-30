5bab4c362cb01b7855a0258414a5ae9d1f5dbc96
Sujan <sujan_khadgi2000@yahoo.com>
2015-09-30 16:10:20 -0400

    Added Slider

--- demo/FYT/js/custom.js
+++ demo/FYT/js/custom.js
@@ -3,3 +3,12 @@ $('.grid').masonry({
   // options
   itemSelector: '.grid-item'
 });
+$(document).ready(function() {
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToShow: 1,
+		variableWidth:true,
+		arrows:false
+	});
+});
