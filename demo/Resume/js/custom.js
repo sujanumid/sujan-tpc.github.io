16bf0643ac67455aa4cf0d4079cb9c5a1ef069de
Sujan <sujan_khadgi2000@yahoo.com>
2015-09-30 18:07:20 -0400

    WIP

---
+++ demo/Resume/js/custom.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+// var sound = new Howl({
+//   urls: ['assets/POL-miracle-park-long.wav',],
+//   autoplay: true,
+//   loop: true,
+//   volume: 0.5,
+// }).play();
+
+// $('.music button').on('click',function(e){
+// 	if($(this).hasClass('play')){
+// 		sound.pause();
+// 		$(this).addClass('pause');
+// 		$(this).removeClass('play');
+// 	}else{
+// 		sound.play();
+// 		$(this).removeClass('pause');
+// 		$(this).addClass('play');
+// 	}
+// });
