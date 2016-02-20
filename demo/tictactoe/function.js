03d50dc040549cbbc7128e855890156a0be06c67
Sujan <sujan_khadgi2000@yahoo.com>
2016-02-19 20:22:01 -0500

    TicTacToe
    Made it algorithmic

--- demo/tictactoe/function.js
+++ demo/tictactoe/function.js
@@ -1,34 +1,99 @@
 var primary = "X",
     secondary = "O",
     thisOnBoard,
-    cell = document.getElementsByClassName('cell'),
-    cellLength = cell.length,
-    noOfx = noOfo = 0;
-var ctx = 9;
+    grid = 3,
+    move = 0;
+board = document.getElementById('game')
+rows = board.getElementsByTagName("tr");
+setup();
 
-winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]];
 
-for(i=0;i<cellLength;i++){
-    cell[i].addEventListener('click',function() {
+function setup(param){
+    console.log(param)
+    rowLength = rows.length;
+    for(i=0;i<rowLength;i++){
+        cellCount = board.rows[i].cells
+        for(j=0;j<cellCount.length;j++){
+            //checks if reset button was pressed
+            if(param){
+                board.rows[i].cells[j].innerHTML = "";
+                move = 0;
+            }else{
+                cell = board.rows[i].cells[j];
+                cell.onclick = function(){
+                    var cellIndex  = this.cellIndex;
+                    var rowIndex = this.parentNode.rowIndex;
+                    playMove(this,cellIndex,rowIndex);
+                }
+            }
+        }
+    }
+}
+
+
+function evaluate(turn,rowIndex,cellIndex){
+    counterX = counterY = counterDiag =counterAnti= 0;
+    move++;
+    for(x=0;x<grid;x++){
+        //check row
+        if(board.rows[rowIndex].cells[x].innerHTML == turn){
+            if(counterX < (grid-1)){
+                counterX++;
+            }else{
+                gameSet(turn)
+            }
+        }
+        //check col
+        if(board.rows[x].cells[cellIndex].innerHTML == turn){
+            if(counterY < (grid-1)){
+                counterY++;
+            }else{
+                gameSet(turn)
+            }
+        }
+        //check diag
+        if(board.rows[x].cells[x].innerHTML == turn){
+            if(counterDiag < (grid-1)){
+                counterDiag++;
+            }else{
+                gameSet(turn)
+            }
+        }
+        //check anti diag
+        rowVal = (grid-1)-x;
+        if(board.rows[rowVal].cells[x].innerHTML == turn){
+            if(counterAnti < (grid-1)){
+                counterAnti++;
+            }else{
+                gameSet(turn)
+            }
+        }
+    }
+    //check draw
+    if(move == Math.pow(grid,2)){
+        gameSet(false);
+    }
+}
+
+function gameSet(turn){
+    if(turn){
+        alert(turn+' wins the game. Play again?')
+    }else{
+        alert('Game ended in a draw. Play again?')
+    }
+    setup(true);
+}
+
+
+function playMove(thisCell,cellIndex,rowIndex){
+    if(thisCell.innerHTML == ""){
         setVal = swapxo();
-        this.setAttribute("type",setVal);
-
-        if(this.innerHTML == ""){
-            this.innerHTML = setVal;
-        }
-
-        if(this.innerHTML === "X"){
-            noOfx++
-        }else{
-            noOfo++
-        }
-        if(noOfx > 2 || noOfo > 2){
-            evaluate();
-        }
-        ctx--;
-        console.log(document.querySelectorAll('type').value);
-    });
-    cell[i].setAttribute("id",i+1);
+        thisCell.setAttribute("type",setVal);
+        thisCell.innerHTML = setVal;
+    } else{
+        console.log('pick another box')
+    }
+    evaluate(setVal,rowIndex,cellIndex)
 }
 
 function swapxo(){
@@ -38,78 +103,3 @@ function swapxo(){
     return thisOnBoard;
 }
 
-
-function evaluate(){
-    if(ctx > 0){
-        if(
-        document.getElementById('1').getAttribute('type') == "X" &&
-        document.getElementById('2').getAttribute('type') == "X" &&
-        document.getElementById('3').getAttribute('type') == "X" ||
-        document.getElementById('4').getAttribute('type') == "X" &&
-        document.getElementById('5').getAttribute('type') == "X" &&
-        document.getElementById('6').getAttribute('type') == "X" ||
-        document.getElementById('7').getAttribute('type') == "X" &&
-        document.getElementById('8').getAttribute('type') == "X" &&
-        document.getElementById('9').getAttribute('type') == "X" ||
-        document.getElementById('1').getAttribute('type') == "X" &&
-        document.getElementById('4').getAttribute('type') == "X" &&
-        document.getElementById('7').getAttribute('type') == "X" ||
-        document.getElementById('2').getAttribute('type') == "X" &&
-        document.getElementById('5').getAttribute('type') == "X" &&
-        document.getElementById('8').getAttribute('type') == "X" ||
-        document.getElementById('3').getAttribute('type') == "X" &&
-        document.getElementById('6').getAttribute('type') == "X" &&
-        document.getElementById('9').getAttribute('type') == "X" ||
-        document.getElementById('1').getAttribute('type') == "X" &&
-        document.getElementById('5').getAttribute('type') == "X" &&
-        document.getElementById('9').getAttribute('type') == "X" ||
-        document.getElementById('7').getAttribute('type') == "X" &&
-        document.getElementById('5').getAttribute('type') == "X" &&
-        document.getElementById('3').getAttribute('type') == "X"
-        ){
-            alert("X won\nDo you want to play another game?");
-            resetBoard();
-        }else if(
-        document.getElementById('1').getAttribute('type') == "O" &&
-        document.getElementById('2').getAttribute('type') == "O" &&
-        document.getElementById('3').getAttribute('type') == "O" ||
-        document.getElementById('4').getAttribute('type') == "O" &&
-        document.getElementById('5').getAttribute('type') == "O" &&
-        document.getElementById('6').getAttribute('type') == "O" ||
-        document.getElementById('7').getAttribute('type') == "O" &&
-        document.getElementById('8').getAttribute('type') == "O" &&
-        document.getElementById('9').getAttribute('type') == "O" ||
-        document.getElementById('1').getAttribute('type') == "O" &&
-        document.getElementById('4').getAttribute('type') == "O" &&
-        document.getElementById('7').getAttribute('type') == "O" ||
-        document.getElementById('2').getAttribute('type') == "O" &&
-        document.getElementById('5').getAttribute('type') == "O" &&
-        document.getElementById('8').getAttribute('type') == "O" ||
-        document.getElementById('3').getAttribute('type') == "O" &&
-        document.getElementById('6').getAttribute('type') == "O" &&
-        document.getElementById('9').getAttribute('type') == "O" ||
-        document.getElementById('1').getAttribute('type') == "O" &&
-        document.getElementById('5').getAttribute('type') == "O" &&
-        document.getElementById('9').getAttribute('type') == "O" ||
-        document.getElementById('7').getAttribute('type') == "O" &&
-        document.getElementById('5').getAttribute('type') == "O" &&
-        document.getElementById('3').getAttribute('type') == "O"
-        ){
-            alert("0 won\nDo you want to play another game?");
-            resetBoard();
-        }
-    }else{
-        alert("Game ended in draw\nDo you want to play another game?");
-        resetBoard();
-    }
-
-}
-
-function resetBoard(){
-    ctx = 9;
-    noOfx = noOfo = 0;
-    for(i=0;i<cellLength;i++){
-        cell[i].innerHTML = "";
-        cell[i].removeAttribute('type')
-    }
-}
