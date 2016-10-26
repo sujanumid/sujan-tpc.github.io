6e5d53c71c680e9ade8bea3935de1599bca56cf3
Sujan <sujan_khadgi2000@yahoo.com>
2016-10-26 19:00:35 -0400

    Added Pages
    -Added Notifications, Chat pages.
    -Added Profile and Search tab content
    -Cropped user pictures

--- demo/pearable/js/my-app.js
+++ demo/pearable/js/my-app.js
@@ -5,7 +5,7 @@ var myApp = new Framework7();
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = myApp.addView('.view', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
@@ -13,3 +13,41 @@ var mainView = myApp.addView('.view-main', {
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
 });
+
+// Conversation flag
+var conversationStarted = false;
+ 
+// Init Messages
+var myMessages = myApp.messages('.messages', {
+  autoLayout:true
+});
+ 
+// Init Messagebar
+var myMessagebar = myApp.messagebar('.messagebar');
+ 
+// Handle message
+$$('.messagebar .link').on('click', function () {
+  // Message text
+  var messageText = myMessagebar.value().trim();
+  // Exit if empy message
+  if (messageText.length === 0) return;
+ 
+  // Empty messagebar
+  myMessagebar.clear()
+  // Add message
+  myMessages.addMessage({
+    // Message text
+    text: messageText,
+    // Random message type
+    type: 'sent',
+    // Avatar and name:
+    avatar: avatar,
+    name: name,
+    // Day
+    day: !conversationStarted ? 'Today' : false,
+    time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
+  })
+ 
+  // Update conversation flag
+  conversationStarted = true;
+});              
